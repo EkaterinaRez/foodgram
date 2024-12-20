21f4b5695a3847b99885085d08701f8bd061b652
Ekaterina Rezvova <katyainapp@gmail.com>
2024-12-20 13:43:56 +0300

    Переписан редирект для урла

--- backend/shortlink/views.py
+++ backend/shortlink/views.py
@@ -7,7 +7,7 @@ def redirect_to_long_url(request, short_url):
     """Переадресация на полный адрес с короткого."""
 
     try:
-        url_short = get_object_or_404(UrlShort, short_url=short_url)
-        return redirect(url_short.long_url)
+        long_url = get_object_or_404(UrlShort, short_url=short_url).long_url
+        return redirect(long_url)
     except UrlShort.DoesNotExist:
         return HttpResponse("Link not found.", status=404)
