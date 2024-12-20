ea3b7688e5230c85c9a72abffc6b72c7ca95dea9
Ekaterina Rezvova <katyainapp@gmail.com>
2024-12-20 14:15:43 +0300

    Файл изменен на txt

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,9 +3,6 @@ from django.db import models as d_models
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfgen import canvas
 from rest_framework import status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
