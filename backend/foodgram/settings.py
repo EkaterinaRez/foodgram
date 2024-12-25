45f8576067a1ecbf081e4163c770c02c556766ae
Ekaterina Rezvova <katyainapp@gmail.com>
2024-12-25 14:22:46 +0300

    debug

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -10,7 +10,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv("SECRET_KEY", "some_fake_secret_to_pass_tests")
 
-DEBUG = os.getenv("DEBUG", "false").strip().lower() == "true"
+# DEBUG = os.getenv("DEBUG", "false").strip().lower() == "true"
+DEBUG = True
 
 ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost,127.0.0.1").split(",")
 
